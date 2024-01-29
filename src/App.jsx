cd94351cf170f966c3bd27143c5f906cadda4602
Arthpatel55 <arth.patel@bypeopletechnologies.com>
2024-01-29 14:16:35 +0530

    Styling the Modal via Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const[projectState,setProjectState] = useState({
     // currentAction:'nothing-selected',
     selectedProjectId: undefined,
-    project:[]
+    projects:[]
   });
   function handleStartAddProject(){
     setProjectState(prevState=>{
@@ -17,29 +17,40 @@ function App() {
       }
     });
   }
+  function handleCancelAddProject(){
+    setProjectState(prevState=>{
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    });
+  }
   function handleAddProject(projectData){
     setProjectState(prevState=>{
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       }
       return{
         ...prevState,
-        project:[...prevState.project,newProject]
+        selectedProjectId : undefined,
+        projects:[...prevState.projects,newProject]
       }
     })
   }
-  console.log(projectState);
+  // console.log(projectState);
   let content; 
   if(projectState.selectedProjectId=== null){
-    content=<NewProject onAdd={handleAddProject} />;
+    content=<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
   }else if(projectState.selectedProjectId===undefined){
     content=<NoProjectSelected onStartAddProject={handleStartAddProject}/>;
   }
   return (
     <main className="h-screen my-8 flex gap-8 " >
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} 
+      projects={projectState.projects} />
       {content}
       {/* <NewProject/> */}
     </main>
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
+
 import React, { useRef } from 'react'
 import Input from './Input.jsx'
+import Model from './Model.jsx';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd , onCancel}) {
+    const model= useRef();   
     const title= useRef();
     const description =useRef();
     const dueDate = useRef();
@@ -10,7 +13,12 @@ export default function NewProject({onAdd}) {
         const enteredTitle = title.current.value;
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
-    
+        if(enteredTitle.trim()===''||
+         enteredDescription.trim()===''|| 
+         enteredDueDate===''){
+            model.current.open();
+            return;
+        }
         onAdd({
             title : enteredTitle,
             description : enteredDescription,
@@ -21,9 +29,18 @@ export default function NewProject({onAdd}) {
     
     return (
     <>
+    <Model ref={model} buttonCaption="Okay" >
+        
+        <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+            Oops ... looks like you forgot to enter a value.</p>
+        <p className="text-stone-600 mb-4">
+            Please make sure you provide a valid value for every input field.</p>
+    </Model>
     <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4" >
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button className="text-stone-800 hover:text-stone-950" 
+            onClick={onCancel}>Cancel</button></li>
             <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
             onClick={handleSave}
             >Save</button></li>
